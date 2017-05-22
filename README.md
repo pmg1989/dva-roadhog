2de8b57cb6e42d5b9ce6b130f3bd8748686517c5
pmg1989 <972401854@qq.com>
2017-05-22 10:21:41 +0800

    首次迁入

---
+++ README.md
@@ -0,0 +1,17 @@
+## antd-mobile demo with roadhog, dva
+
+Include below common usage:
+
+- custom svg config
+- rem config
+- custom theme config
+
+### dev
+
+```
+npm install
+npm start
+```
+
+http://localhost:8000/
+
---
+++ src/routes/IndexPage.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-plusplus, global-require */
+import React from 'react';
+import PropTypes from 'prop-types'
+import { connect } from 'dva';
+import { DatePicker, List, Button, Icon, Tabs, WhiteSpace } from 'antd-mobile';
+
+const Index = () => {
+  return (
+    <div>haha2222</div>
+  )
+}
+
+Index.propTypes = {
+
+}
+
+export default connect()(Index);
