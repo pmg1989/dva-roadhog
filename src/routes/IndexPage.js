7d81219456ebf98fc900ae1ebce6ebabd2e8b0f8
pmg1989 <972401854@qq.com>
2017-05-22 11:54:43 +0800

    fix eslint semi

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-plusplus, global-require */
-import React from 'react';
+import React from 'react'
 // import PropTypes from 'prop-types'
-import { connect } from 'dva';
+import { connect } from 'dva'
 // import { DatePicker, List, Button, Icon, Tabs, WhiteSpace } from 'antd-mobile';
 
 const Index = () => {
   return (
-    <div>haha2222</div>
-  );
-};
+    <div>haha</div>
+  )
+}
 
 Index.propTypes = {
 
-};
+}
 
-export default connect()(Index);
+export default connect()(Index)
