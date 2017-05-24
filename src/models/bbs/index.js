f85d67b949fe2a9e25bd5d03a2e3b8428b223789
pmg1989 <972401854@qq.com>
2017-05-24 18:03:02 +0800

    fix bbs index

--- src/models/bbs/index.js
+++ src/models/bbs/index.js
@@ -1,26 +1,61 @@
-import { query } from '../../services/bbs/index'
+import { getCategory, getList } from '../../services/bbs/index'
+
+const cateList = ['post_desc', 'hot_first', 'near_most'] // 每个类别的ID号
 
 export default {
   namespace: 'bbsIndex',
   state: {
     navOpen: false,
     categories: [],
+    loading: [true, true, true],
+    latest: [],
+    hot: [],
+    near: [],
+    pagination: {
+      page: 1,
+      size: 5,
+    },
   },
   subscriptions: {
     setup({ dispatch, history }) {
       history.listen((location) => {
         const pathname = location.pathname
         if (pathname === '/' || pathname === '/bbs/index') {
-          dispatch({ type: 'query' })
+          dispatch({ type: 'queryCategory' })
+          dispatch({ type: 'queryList', payload: { tab: +location.query.tab || 0 } })
         }
       })
     },
   },
   effects: {
-    *query({ payload }, { call, put }) {
-      const data = yield call(query)
+    *queryCategory({ payload }, { call, put }) {
+      const data = yield call(getCategory)
       if (data.success) {
         yield put({ type: 'querySuccess', payload: { categories: data.categories } })
+      }
+    },
+    *queryList({ payload }, { call, put, select }) {
+      const pagination = yield select(state => state.bbsIndex.pagination)
+
+      const data = yield call(getList, {
+        filter: cateList[payload.tab],
+        ...pagination,
+      })
+
+      if (data.success) {
+        switch (payload.tab) {
+          case 0:
+            yield put({ type: 'querySuccess', payload: { latest: data.posts } })
+            break
+          case 1:
+            yield put({ type: 'querySuccess', payload: { hot: data.posts } })
+            break
+          case 2:
+            yield put({ type: 'querySuccess', payload: { near: data.posts } })
+            break
+          default:
+            break
+        }
       }
     },
   },
--- src/services/bbs/index.js
+++ src/services/bbs/index.js
@@ -1,7 +1,14 @@
 import { request } from '../../utils'
 
-export async function query(params) {
+export async function getCategory(params) {
   return request('/api/bbs/categories', {
+    method: 'get',
+    data: params,
+  })
+}
+
+export async function getList(params) {
+  return request('/api/bbs/posts', {
     method: 'get',
     data: params,
   })
