6735b01fbea1da1a8439fceee8e312c0d16f2203
pmg1989 <972401854@qq.com>
2017-05-31 17:27:06 +0800

    fix bbsIndex

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ function RouterConfig({ history }) {
     <Router history={history} component={require('./routes/App')}>
       <Route path="/" component={require('./routes/bbs/Index')} />
       <Route path="bbs/index" component={require('./routes/bbs/Index')} />
+      <Route path="bbs/category" component={require('./routes/bbs/Category')} />
       <Route path="bbs/detail" component={require('./routes/bbs/Detail')} />
       <Route path="bbs/add" component={require('./routes/bbs/Add')} />
       <Route path="*" component={require('./routes/Error')} />
---
+++ src/routes/bbs/Category/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'dva'
+import { Flex } from 'antd-mobile'
+import { Header } from '../../../components'
+
+const Add = () => {
+  // const { query: { token } } = location
+
+  const headerProps = {
+    rightContent: (
+      <Flex>
+        <Flex className="navbar-right-content">发布</Flex>
+      </Flex>
+    ),
+  }
+
+  return (
+    <div>
+      <Header {...headerProps}>发布帖子</Header>
+    </div>
+  )
+}
+
+export default connect()(Add)
