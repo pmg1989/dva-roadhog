f0f72b437602c65a0e3c9d91d4951117a51f1a76
pmg1989 <972401854@qq.com>
2017-06-21 16:28:27 +0800

    fix CompetitionWork

--- src/routes/festival/Competition/List.js
+++ src/routes/festival/Competition/List.js
@@ -5,6 +5,14 @@ import styles from './List.less'
 
 const TabPane = Tabs.TabPane
 
+const Rank = ({ rank }) => {
+  return (
+    <span className={styles.rank}>
+      <Icon type={require(`../../../svg/festival/No.${rank}.svg`)} />
+    </span>
+  )
+}
+
 const List = () => {
   function callback(key) {
     console.log('onChange', key)
@@ -25,7 +33,151 @@ const List = () => {
                 <div className={styles.mask_box}>
                   <span>Jasson</span>
                 </div>
-                <span className={styles.rank} />
+                <Rank rank={1} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={2} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={3} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={4} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={5} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={6} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={7} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={8} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={9} />
+              </div>
+              <div className={styles.bottom}>
+                <div className={styles.vote_icon}>
+                  <span className={styles.icon}><Icon type={require('../../../svg/festival/vote.svg')} /></span>
+                  <span>12</span>
+                </div>
+                <Button className={styles.btn_vote}>投票</Button>
+              </div>
+            </li>
+            <li>
+              <div className={styles.top}>
+                <img className={styles.thumb} alt="" src="http://res.cakeland.com/item/米奇系列（优酪慕斯）/260x260/4.png" />
+                <div className={styles.mask_box}>
+                  <span>Jasson</span>
+                </div>
+                <Rank rank={10} />
               </div>
               <div className={styles.bottom}>
                 <div className={styles.vote_icon}>
@@ -45,7 +197,7 @@ const List = () => {
                 <div className={styles.mask_box}>
                   <span>Jasson</span>
                 </div>
-                <span className={styles.rank} />
+                <Rank rank={1} />
               </div>
               <div className={styles.bottom}>
                 <div className={styles.vote_icon}>
