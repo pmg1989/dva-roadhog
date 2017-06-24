bea99b5ee300e0b504e4deba1c5be4606be20a91
pmg1989 <972401854@qq.com>
2017-06-24 17:57:49 +0800

    fix audioPlayer

--- src/routes/festival/PracticeWork/Content.js
+++ src/routes/festival/PracticeWork/Content.js
@@ -5,7 +5,6 @@ import { AudioPlayer } from 'NbComponent'
 import styles from './Content.less'
 
 const Content = () => {
-
   const audioPlayerProps = {
     source: 'https://o9u2lnvze.qnssl.com//practice_songd7a71296-42f5-40b5-bacb-141ea5d139b8.mp3',
     lrc: 'https://o9u2lnvze.qnssl.com//competition5a1e4747-2e3e-48ba-bba8-8b08b462d9c8.lrc',
@@ -14,7 +13,7 @@ const Content = () => {
   return (
     <div className={styles.content_box}>
       <div className={styles.top}>
-        <AudioPlayer {...audioPlayerProps}/>
+        <AudioPlayer {...audioPlayerProps} />
       </div>
     </div>
   )
