e08b88060b90ca1fe88d0c7e478665c8c3a5530c
pmg1989 <972401854@qq.com>
2017-06-24 19:45:40 +0800

    fix audioPlayer

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
 import { Slider, Icon } from 'antd-mobile'
 import $ from 'jQuery'
 import lyric from './lyric'
@@ -35,6 +36,7 @@ class AudioPlayer extends Component {
       totalTime: 0,
       percent: 0,
       lyricList: [],
+      lrcStatus: true, //true： 两行歌词；false：多行歌词
     }
   }
 
@@ -55,19 +57,20 @@ class AudioPlayer extends Component {
 
     player.addEventListener('timeupdate', (e) => {
       const { currentTime, duration } = e.target
-
+      let oldTop = this.state.lrcStatus ? 0 : 200
       this.state.lyricList.forEach((item, i) => {
         if (currentTime > item[0] - 0.5) { /* preload the lyric by 0.50s*/
           const line = $(`#line-${i}`)
           line.addClass('current-line-1').siblings('p').removeClass('current-line-1')
-          lyricContainer.style.top = `-${line[0].offsetTop}px`
+          lyricContainer.style.top = oldTop - line[0].offsetTop + 'px'//`(100 - ${line[0].offsetTop})px`
         }
       })
       this.setState({ currentTime, percent: (currentTime / duration) * 100 })
     })
   }
 
-  handlePlayPause() {
+  handlePlayPause(e) {
+    e.stopPropagation()
     if (this.state.isPlay) {
       player.pause()
     } else {
@@ -78,8 +81,14 @@ class AudioPlayer extends Component {
     }))
   }
 
+  changeLrcType() {
+    this.setState(prevState => ({
+      lrcStatus: !prevState.lrcStatus,
+    }))
+  }
+
   render() {
-    const { lyricList, percent, currentTime, totalTime, isPlay } = this.state
+    const { lyricList, percent, currentTime, totalTime, isPlay, lrcStatus } = this.state
     const { source } = this.props
 
     const slideProps = {
@@ -106,20 +115,21 @@ class AudioPlayer extends Component {
     }
 
     return (
-      <div className={styles.box}>
+      <div className={styles.box} onClick={::this.changeLrcType}>
         <div className={styles.container}>
-          <div className={styles.lyric_wrapper}>
+          <div className={classnames(styles.lyric_wrapper, { [styles.nomarl] : !lrcStatus })}>
             <div className={styles.lyric_container} id="lyricContainer">
               {lyricList.map((item, key) => (
                 <p key={key} id={`line-${key}`}>{item[1]}</p>
               ))}
+              {lyricList.length === 0 && <p>歌词加载中...</p>}
             </div>
             <div className={styles.opt_box} onClick={::this.handlePlayPause}>
               {!isPlay && <Icon type={require('../../svg/festival/play.svg')} />}
               {isPlay && <Icon type={require('../../svg/festival/pause.svg')} />}
             </div>
           </div>
-          <div className={styles.slider_box}>
+          <div className={styles.slider_box} onClick={(e) => e.stopPropagation()}>
             <span className={styles.curtime}>{AudioPlayer.parseTime(currentTime)}</span>
             <Slider {...slideProps} />
             <span className={styles.duration}>{AudioPlayer.parseTime(totalTime)}</span>
