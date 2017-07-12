7fa49f10cfd105264994e9e7550e9bcd881ef4aa
pmg1989 <972401854@qq.com>
2017-07-12 10:34:12 +0800

    fix bbs add send

--- src/routes/bbs/Add/index.js
+++ src/routes/bbs/Add/index.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { connect } from 'dva'
+import classnames from 'classnames'
 import { Flex } from 'antd-mobile'
 import { Header } from 'NbComponent'
 import Content from './Content'
+import styles from './Content.less'
 
 const Add = ({ dispatch, bbsAdd }) => {
   const { categories, selected, item } = bbsAdd
 
+  const addStatus = !!item.bbsCategory.cid && !!item.title.length && item.content.length > 8
+
+  const handleAdd = () => {
+    addStatus && dispatch({ type: 'bbsAdd/addSend' })
+  }
+
   const header1Props = {
     rightContent: (
       <Flex>
-        <Flex className="navbar-right-content" onClick={() => {
-          dispatch({ type: 'bbsAdd/addSend' })
-        }}
-        >发布</Flex>
+        <Flex className="navbar-right-content">
+          <span className={classnames({ [styles.disabled]: !addStatus })} onClick={handleAdd}>发布</span>
+        </Flex>
       </Flex>
     ),
   }
