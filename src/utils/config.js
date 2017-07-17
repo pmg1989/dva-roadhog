d9b9cf16a1c0898f88ca9c6d72d8bf6fd077f001
pmg1989 <972401854@qq.com>
2017-07-17 10:20:33 +0800

    fix config env

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,23 +3,23 @@ const hostname = location.hostname
 
 function getEnv() {
   return ['localhost',
-          '0.0.0.0',
-          '127.0.0.1',
-          '192.168.1.148',
-          'test.newband.com'
-         ].indexOf(hostname) > -1
+    '0.0.0.0',
+    '127.0.0.1',
+    '192.168.1.148',
+    'test.newband.com',
+  ].indexOf(hostname) > -1
 }
 
 function getBaseURL() {
-  if(getEnv()) {
+  if (getEnv()) {
     return 'http://ec2-54-223-130-122.cn-north-1.compute.amazonaws.com.cn:81'
   }
   return 'https://api.students.newband.com'
 }
 
-export default  {
+export default {
   logoSrc: 'https://o9u2lnvze.qnssl.com/web/global/brand.png',
   baseURL: getBaseURL(),
   env: getEnv() ? 'pro' : 'staging',
-  refreshTime: getEnv() ? 300 : 100
+  refreshTime: getEnv() ? 300 : 100,
 }
