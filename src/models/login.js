f48f4115a60091d20bc16ebedecec3b097db3845
pmg1989 <972401854@qq.com>
2017-07-18 10:58:26 +0800

    fix Login

--- src/models/login.js
+++ src/models/login.js
@@ -1,10 +1,18 @@
 
+// import { queryString } from 'utils/tools'
+
 export default {
   namespace: 'login',
   state: {
   },
   subscriptions: {
     // setup({ dispatch, history }) {
+    //   history.listen((location) => {
+    //     const { pathname } = location
+    //     if (pathname === '/login') {
+    //       console.log(queryString('url'))
+    //     }
+    //   })
     // },
   },
   effects: {
