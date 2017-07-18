623baf2f0b0f40177f8ed31b49af411dfe366165
pmg1989 <972401854@qq.com>
2017-07-18 09:46:10 +0800

    fix shareBaseURL

--- src/utils/config.js
+++ src/utils/config.js
@@ -17,9 +17,18 @@ function getBaseURL() {
   return 'https://api.students.newband.com'
 }
 
+function getBaseShare() {
+  if(getEnv()) {
+    return 'http://staging.web.newband.com:5000/api/v1/social/wxcfg'
+  }
+
+  return 'http://web.newband.com/api/v1/social/wxcfg'
+}
+
 export default {
   logoSrc: 'https://o9u2lnvze.qnssl.com/web/global/brand.png',
   baseURL: getBaseURL(),
+  baseShare: getBaseShare(),
   env: getEnv() ? 'pro' : 'staging',
   refreshTime: getEnv() ? 300 : 100,
 }
